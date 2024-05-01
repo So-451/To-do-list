1422a2578ccc486ae8927aadb074b0508d9481ca
So-451 <sarkarsolanki045@gmail.com>
2024-05-02 01:10:43 +0530

    Front End

---
+++ src/components/Author/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {AuthorContainer} from './styles'
+
+function Author() {
+  return (
+    <AuthorContainer> Created by .......</AuthorContainer>
+  )
+}
+
+export default Author
